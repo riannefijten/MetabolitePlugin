cceba414b32bfd01d41ae86e871a5ad8c83d8314
Rianne Fijten <riannefijten@gmail.com>
2012-05-31 10:00:50 +0200

    made different methods for each separate panel. Also included a table for the metabolite information display.

--- src/metaboliteplugin/MetaboliteInfoGUI.java
+++ src/metaboliteplugin/MetaboliteInfoGUI.java
@@ -6,13 +6,16 @@ package metaboliteplugin;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.table.TableColumn;
 
 import org.pathvisio.desktop.PvDesktop;
 import org.pathvisio.desktop.plugin.Plugin;
@@ -21,44 +24,95 @@ public class MetaboliteInfoGUI implements Plugin
 {
 	private PvDesktop desktop;
 
-	public void init(PvDesktop desktop)
-	{
-		//Create sub-panel for structure formula
-		JPanel GeneralPanelSub1 = new JPanel();
-			GeneralPanelSub1.add(new JLabel("Structure Formula"));
-			GeneralPanelSub1.setBorder(BorderFactory.createLineBorder(Color.CYAN));
-		//Create sub-panel for metabolite information
+	public Component sub1(){
+		//Create sub-panel for structure image
 		JPanel GeneralPanelSub2 = new JPanel();
-			GeneralPanelSub2.add(new JLabel("Metabolite info"));
-			GeneralPanelSub2.setBorder(BorderFactory.createLineBorder(Color.CYAN));
+		GeneralPanelSub2.add(new JLabel("Structure image"));
+		GeneralPanelSub2.setBorder(BorderFactory.createLineBorder(Color.CYAN));
+		return GeneralPanelSub2;
+	}
+	public Component sub2(){	//Create sub-panel for metabolite information
+		
+		//Create table for info
+		String [] columnNames = {"Metabolite info", " "};
+		Object [][] data = {{"CAS-number:", "nklsg"},{"Structure formula:", "C3H5O8"}};
+		JTable table = new JTable(data, columnNames);
+		
+		//Disable auto-resize of table
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		
+		//Set column width of first table column
+		int vColIndex = 0;
+		TableColumn col = table.getColumnModel().getColumn(vColIndex);
+		int width = 100;
+		col.setPreferredWidth(width);
+		
+		//Create panel for metabolite info
+		JPanel GeneralPanelSub1 = new JPanel();
+		GeneralPanelSub1.add(table);
+		GeneralPanelSub1.setBorder(BorderFactory.createLineBorder(Color.CYAN));
+		GeneralPanelSub1.setBackground(Color.white);
+		
+		//Set size of the panel to ensure enough room for both panels
+		Dimension panelD = new Dimension(130, 100);  
+		GeneralPanelSub1.setPreferredSize(panelD);
+		
+		return GeneralPanelSub1;
+	}
+	public Component GeneralPanel(){
+		
 		//Create panel for general information about the metabolite.
 		JPanel GeneralPanel = new JPanel();
-			GeneralPanel.setLayout(new BoxLayout(GeneralPanel, BoxLayout.X_AXIS));
-			GeneralPanel.add(GeneralPanelSub1); //TODO instead of labels, implement CDK
-			GeneralPanel.add(GeneralPanelSub2); //TODO instead of labels, implement CDK
-			GeneralPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		GeneralPanel.setLayout(new BoxLayout(GeneralPanel, BoxLayout.X_AXIS));
+		GeneralPanel.add(sub1()); //TODO instead of labels, implement CDK
+		GeneralPanel.add(sub2()); //TODO instead of labels, implement CDK
+		GeneralPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		
+		return GeneralPanel;
+	}
+	public Component MSPanel(){
+		
 		//Create panel for MS data/information
 		JPanel MSPanel = new JPanel();
-			MSPanel.add(new JLabel("MS info")); //TODO instead of labels, implement MS data
-			MSPanel.setBorder(BorderFactory.createLineBorder(Color.RED));
+		MSPanel.add(new JLabel("MS info")); //TODO instead of labels, implement MS data
+		MSPanel.setBorder(BorderFactory.createLineBorder(Color.RED));
+		
+		return MSPanel;
+	}
+	public Component NMRPanel(){
+		
 		//Create panel for NMR data/information
 		JPanel NMRPanel = new JPanel();
-			NMRPanel.add(new JLabel("NMR info")); //TODO instead of labels, implement NMR data
-			NMRPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
-			
+		NMRPanel.add(new JLabel("NMR info")); //TODO instead of labels, implement NMR data
+		NMRPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+		
+		return NMRPanel;
+	}
+	public void init(PvDesktop desktop)
+	{
+		// TODO if datanode type equals metabolite{
+				
 		// create side bar
-		JPanel InfoPanel = new JPanel ();
-		JScrollPane scroller = new JScrollPane(InfoPanel);
-		InfoPanel.setLayout (new BoxLayout(InfoPanel, BoxLayout.PAGE_AXIS));
-		InfoPanel.add (new JLabel ("Metabolite information will be shown here."), BorderLayout.LINE_START);
-		InfoPanel.add(GeneralPanel);
-		InfoPanel.add(MSPanel);
-		InfoPanel.add(NMRPanel);
+		JPanel scroller = new JPanel();
+		
+		scroller.setLayout (new BoxLayout(scroller, BoxLayout.PAGE_AXIS));
+		scroller.add (new JLabel ("Metabolite information will be shown here."), BorderLayout.LINE_START);
+		scroller.add(GeneralPanel());
+		scroller.add(MSPanel());
+		scroller.add(NMRPanel());
+		
 		// get a reference to the sidebar
 		JTabbedPane sidebarTabbedPane = desktop.getSideBarTabbedPane();
+		
 		// add side bar title
-		sidebarTabbedPane.add("Metabolite Information", InfoPanel);
-				}
+		sidebarTabbedPane.add("Metabolite Information", scroller);
+		
+	}
 
-	public void done() {}
+	@Override
+	public void done() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
