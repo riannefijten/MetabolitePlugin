29c8932012f43a51046d6e406559d6dbda255eb8
Rianne Fijten <riannefijten@gmail.com>
2012-06-17 15:18:44 +0200

    Added standard inchi, fixed some visualization and aligment problems

--- src/metaboliteplugin/MetaboliteInfoGUI.java
+++ src/metaboliteplugin/MetaboliteInfoGUI.java
@@ -8,39 +8,40 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
-import javax.swing.SwingConstants;
+import javax.swing.JTextArea;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
-import org.openscience.cdk.io.MDLV2000Reader;
-import org.openscience.cdk.io.formats.MDLV2000Format;
-import org.openscience.cdk.templates.MoleculeFactory;
 import org.pathvisio.desktop.PvDesktop;
 import org.pathvisio.desktop.plugin.Plugin;
 
 public class MetaboliteInfoGUI implements Plugin
 {
 	private PvDesktop desktop;
+	
+	private final JLabel empty  = new JLabel(" ");
 	
 	//String for Cactus service
 	private static final String SERVICE = "http://cactus.nci.nih.gov/chemical/structure/";
@@ -57,7 +58,7 @@ public class MetaboliteInfoGUI implements Plugin
 		try {
 		//Set up connection and put InChI key into a string
 			HttpClient httpclient = new DefaultHttpClient();
-			HttpGet getInchi = new HttpGet(SERVICE + GetName() + "/stdinchikey");
+			HttpGet getInchi = new HttpGet(SERVICE + GetName() + "/stdinchi");
 			
 			HttpResponse response = null;
 			response = httpclient.execute(getInchi);
@@ -75,9 +76,35 @@ public class MetaboliteInfoGUI implements Plugin
 			  System.out.println(throwable.getMessage());
 			  throwable.printStackTrace();
 		}
-		System.out.println(inchiInfo);
 		return inchiInfo;
 		}
+	
+	//Request InChI string from Cactus
+	public static String InchiKey(){
+		String inchiKeyInfo = null;
+		try {
+		//Set up connection and put InChI key into a string
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpGet getInchiKey = new HttpGet(SERVICE + GetName() + "/stdinchikey");
+			
+			HttpResponse response = null;
+			response = httpclient.execute(getInchiKey);
+			
+			HttpEntity entity = response.getEntity();
+			inchiKeyInfo = EntityUtils.toString(entity);
+		
+		} catch (ClientProtocolException ClientException) {
+			System.out.println(ClientException.getMessage());
+			ClientException.printStackTrace();
+		} catch (IOException IoException) {
+			System.out.println(IoException.getMessage());
+			IoException.printStackTrace();
+		} catch (Throwable throwable) {
+			  System.out.println(throwable.getMessage());
+			  throwable.printStackTrace();
+		}
+		return inchiKeyInfo;
+	}
 	
 	//Request structure image from Cactus
 	public Image image(){
@@ -90,18 +117,32 @@ public class MetaboliteInfoGUI implements Plugin
 		
 		Image image = Toolkit.getDefaultToolkit().createImage(imageUrl);
 
-		System.out.println(image);
 		return image;
 	}
 
 	public Component GeneralPanel(){
+
+		String name = "Metabolite name: " + GetName() + "\n \n";
+		String inchiKey = InchiKey();
+		String inchi = Inchi() + "\n \n";
+		String info = name + inchi + inchiKey;
+
+		JLabel image = new JLabel(new ImageIcon(image()));
+		
+		JTextArea area = new JTextArea(info);
+		area.setEditable(false); 
+		area.setLineWrap(true); 
+		area.setWrapStyleWord(true); 
 		
 		//Create panel for general information about the metabolite.
 		JPanel GeneralPanel = new JPanel();
 		GeneralPanel.setBackground(Color.WHITE);
-		GeneralPanel.add(new JLabel("Metabolite name: " + GetName()));		
-		GeneralPanel.add(new JLabel(Inchi()));
-		GeneralPanel.add(new JLabel(new ImageIcon(image())));
+		GeneralPanel.setLayout(new BoxLayout(GeneralPanel, BoxLayout.Y_AXIS));
+
+		GeneralPanel.add(image);
+		image.setAlignmentX(Component.LEFT_ALIGNMENT);
+		GeneralPanel.add(area);
+		area.setAlignmentX(Component.LEFT_ALIGNMENT);
 		
 		return GeneralPanel;
 	}
@@ -135,7 +176,7 @@ public class MetaboliteInfoGUI implements Plugin
 		
 		scroller.setLayout (new BoxLayout(scroller, BoxLayout.PAGE_AXIS));
 		scroller.setBackground(Color.white);
-		scroller.add (new JLabel ("Metabolite information will be shown here."), BorderLayout.PAGE_START);
+		//scroller.add (new JLabel ("Metabolite information will be shown here."), BorderLayout.LINE_START);
 		scroller.add(GeneralPanel());
 		scroller.add(MSPanel());
 		scroller.add(NMRPanel());
