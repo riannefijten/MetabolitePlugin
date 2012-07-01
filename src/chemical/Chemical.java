30ec591929e823f6d9e2c4975d3c99c08b28f3bb
Rianne Fijten <riannefijten@gmail.com>
2012-07-01 18:04:00 +0200

    retrieval of  MS and NMR data from HMDB added

--- src/chemical/Chemical.java
+++ src/chemical/Chemical.java
@@ -1,56 +1,43 @@
 package chemical;
 
-import java.io.File;
 import java.io.IOException;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
-import org.openscience.cdk.AtomContainer;
-import org.openscience.cdk.Molecule;
-import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.interfaces.IMolecule;
-import org.openscience.cdk.io.IChemObjectReader.Mode;
-import org.openscience.cdk.io.MDLV2000Reader;
-import org.openscience.cdk.io.SMILESReader;
-import org.openscience.cdk.layout.StructureDiagramGenerator;
 
 
 public class Chemical {
-	public static String Inchi(){
-	String cid = "methane";
-	HttpClient httpclient = new DefaultHttpClient();
-	HttpGet getInchi = new HttpGet("http://cactus.nci.nih.gov/chemical/structure/" + cid + "/stdinchikey");
-	HttpResponse response = null;
-	String inchiInfo = null;
-	try {
-		response = httpclient.execute(getInchi);
+	public static void Pubchem(){
+		String cid = "5793";
+		String pubChemInchi = null;
+		try {
+		
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet pubChemRequest = new HttpGet("http://pubchem.ncbi.nlm.nih.gov/summary/summary.cgi?cid="
+				+ cid + "5793&loc=ec_rcs");
+		
+		HttpResponse response = null;
+		response = httpclient.execute(pubChemRequest);
+		
 		HttpEntity entity = response.getEntity();
-		inchiInfo = "InChI key of " + cid + ": " + (EntityUtils.toString(entity));
-	} catch (ClientProtocolException e) {
-		inchiInfo = "Exception occured. Request failed.";
-	} catch (IOException e) {
-		inchiInfo = "Exception occured. Request failed.";
+		pubChemInchi = EntityUtils.toString(entity);
+		
+	} catch (ClientProtocolException ClientException) {
+		System.out.println(ClientException.getMessage());
+		ClientException.printStackTrace();
+	} catch (IOException IoException) {
+		System.out.println(IoException.getMessage());
+		IoException.printStackTrace();
+	} catch (Throwable throwable) {
+		  System.out.println(throwable.getMessage());
+		  throwable.printStackTrace();
 	}
-	return inchiInfo;
-	}
-	
-	public static void main(String[] args){
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		JLabel label = new JLabel(Inchi());
-		panel.add(label);
-		frame.add(panel);
-		frame.pack();
-		frame.setVisible(true);
+		System.out.println(pubChemInchi);
+		
 	}
 }
--- src/metaboliteplugin/MetaboliteInfoGUI.java
+++ src/metaboliteplugin/MetaboliteInfoGUI.java
@@ -14,6 +14,7 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.IOException;
+import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -34,6 +35,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.openscience.cdk.nonotify.NNMolecule;
 import org.pathvisio.desktop.PvDesktop;
 import org.pathvisio.desktop.plugin.Plugin;
 
@@ -41,7 +43,8 @@ public class MetaboliteInfoGUI implements Plugin
 {
 	private PvDesktop desktop;
 	
-	private final JLabel empty  = new JLabel(" ");
+	
+	private final String HMDB = "HMDB00122";
 	
 	//String for Cactus service
 	private static final String SERVICE = "http://cactus.nci.nih.gov/chemical/structure/";
@@ -106,8 +109,133 @@ public class MetaboliteInfoGUI implements Plugin
 		return inchiKeyInfo;
 	}
 	
+//	public static String Pubchem(){
+//		String cid = "5793";
+//		String pubChemInchi = null;
+//		try {
+//		
+//		HttpClient httpclient = new DefaultHttpClient();
+//		HttpGet pubChemRequest = new HttpGet("http://pubchem.ncbi.nlm.nih.gov/summary/summary.cgi?cid="
+//				+ cid + "&disopt=SaveXML");
+//		pubChemRequest.getAllHeaders();
+//		System.out.println(pubChemRequest);
+//
+//		HttpResponse response = null;
+//		response = httpclient.execute(pubChemRequest);
+//		HttpEntity entity = response.getEntity();
+//		pubChemInchi = EntityUtils.toString(entity);
+//		System.out.println(pubChemInchi);
+//		
+//	} catch (ClientProtocolException ClientException) {
+//		System.out.println(ClientException.getMessage());
+//		ClientException.printStackTrace();
+//	} catch (IOException IoException) {
+//		System.out.println(IoException.getMessage());
+//		IoException.printStackTrace();
+//	} catch (Throwable throwable) {
+//		  System.out.println(throwable.getMessage());
+//		  throwable.printStackTrace();
+//	}
+//		return pubChemInchi;
+//		
+//	}
+	
+	public Image MSImageLow(){
+		URL imageUrl = null;
+		try {
+			imageUrl = new URL("http://www.hmdb.ca/labm/metabolites/"
+			+ HMDB + "/ms/spectra/" + HMDB + "L.png");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		Image image = Toolkit.getDefaultToolkit().createImage(imageUrl);
+		return image;
+	}
+	
+	public Image MSImageMed(){
+		URL imageUrl = null;
+		try {
+			imageUrl = new URL("http://www.hmdb.ca/labm/metabolites/" 
+			+ HMDB + "/ms/spectraM/" + HMDB + "M.png");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		Image image = Toolkit.getDefaultToolkit().createImage(imageUrl);
+		return image;
+	}
+	
+	public Image MSImageHigh(){
+		URL imageUrl = null;
+		try {
+			imageUrl = new URL("http://www.hmdb.ca/labm/metabolites/" 
+			+ HMDB + "/ms/spectraH/" + HMDB + "H.png");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		Image image = Toolkit.getDefaultToolkit().createImage(imageUrl);
+		return image;
+	}
+	
+	public String h1NMR(){
+		String H1NMR = null;
+
+		try {
+		//Set up connection and put InChI key into a string
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpGet getH1NMR = new HttpGet("http://www.hmdb.ca/labm/metabolites/"
+			+ HMDB + "/chemical/pred_hnmr_peaklist/" + HMDB + "_peaks.txt");
+			
+			HttpResponse response = null;
+			response = httpclient.execute(getH1NMR);
+			
+			HttpEntity entity = response.getEntity();
+			H1NMR = EntityUtils.toString(entity);
+		
+		} catch (ClientProtocolException ClientException) {
+			System.out.println(ClientException.getMessage());
+			ClientException.printStackTrace();
+		} catch (IOException IoException) {
+			System.out.println(IoException.getMessage());
+			IoException.printStackTrace();
+		} catch (Throwable throwable) {
+			  System.out.println(throwable.getMessage());
+			  throwable.printStackTrace();
+		}
+		return H1NMR;
+		
+	}
+	
+	public String c13NMR(){
+		String C13NMR = null;
+		try {
+		//Set up connection and put InChI key into a string
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpGet getC13NMR = new HttpGet("http://www.hmdb.ca/labm/metabolites/"
+			+ HMDB + "/chemical/pred_cnmr_peaklist/" + HMDB + "_peaks.txt");
+			
+			HttpResponse response = null;
+			response = httpclient.execute(getC13NMR);
+			
+			HttpEntity entity = response.getEntity();
+			C13NMR = EntityUtils.toString(entity);
+		
+		} catch (ClientProtocolException ClientException) {
+			System.out.println(ClientException.getMessage());
+			ClientException.printStackTrace();
+		} catch (IOException IoException) {
+			System.out.println(IoException.getMessage());
+			IoException.printStackTrace();
+		} catch (Throwable throwable) {
+			  System.out.println(throwable.getMessage());
+			  throwable.printStackTrace();
+		}
+		return C13NMR;
+	}
 	//Request structure image from Cactus
-	public Image image(){
+	public Image moleculeImage(){
 		URL imageUrl = null;
 		try {
 			imageUrl = new URL(SERVICE + GetName() + "/image");
@@ -125,9 +253,10 @@ public class MetaboliteInfoGUI implements Plugin
 		String name = "Metabolite name: " + GetName() + "\n \n";
 		String inchiKey = InchiKey();
 		String inchi = Inchi() + "\n \n";
-		String info = name + inchi + inchiKey;
+		//String pubchem = Pubchem();
+		String info = name + inchi + inchiKey /* + pubchem*/;
 
-		JLabel image = new JLabel(new ImageIcon(image()));
+		JLabel image = new JLabel(new ImageIcon(moleculeImage()));
 		
 		JTextArea area = new JTextArea(info);
 		area.setEditable(false); 
@@ -137,12 +266,11 @@ public class MetaboliteInfoGUI implements Plugin
 		//Create panel for general information about the metabolite.
 		JPanel GeneralPanel = new JPanel();
 		GeneralPanel.setBackground(Color.WHITE);
+		GeneralPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		GeneralPanel.setLayout(new BoxLayout(GeneralPanel, BoxLayout.Y_AXIS));
-
-		GeneralPanel.add(image);
-		image.setAlignmentX(Component.LEFT_ALIGNMENT);
+		
 		GeneralPanel.add(area);
-		area.setAlignmentX(Component.LEFT_ALIGNMENT);
+		GeneralPanel.add(image);
 		
 		return GeneralPanel;
 	}
@@ -153,6 +281,13 @@ public class MetaboliteInfoGUI implements Plugin
 		JPanel MSPanel = new JPanel();
 		MSPanel.add(new JLabel("MS info")); //TODO instead of labels, implement MS data
 		MSPanel.setBorder(BorderFactory.createLineBorder(Color.RED));
+		MSPanel.setLayout(new BoxLayout(MSPanel, BoxLayout.Y_AXIS));
+		JPanel MSPanel2 = new JPanel();
+		MSPanel.add(MSPanel2);
+		MSPanel2.setLayout(new BoxLayout(MSPanel2, BoxLayout.X_AXIS));
+		MSPanel2.add(new JLabel(new ImageIcon(MSImageLow())));
+		MSPanel2.add(new JLabel(new ImageIcon(MSImageMed())));
+		MSPanel2.add(new JLabel(new ImageIcon(MSImageHigh())));
 		
 		return MSPanel;
 	}
@@ -162,8 +297,21 @@ public class MetaboliteInfoGUI implements Plugin
 		//Create panel for NMR data/information
 		JPanel NMRPanel = new JPanel();
 		NMRPanel.add(new JLabel("NMR info")); //TODO instead of labels, implement NMR data
+		NMRPanel.setLayout(new BoxLayout(NMRPanel, BoxLayout.Y_AXIS));
 		NMRPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
 		
+		JTextArea C13NMR = new JTextArea(c13NMR());
+			C13NMR.setEditable(false);
+			C13NMR.setLineWrap(true); 
+			C13NMR.setWrapStyleWord(true);
+		
+		JTextArea H1NMR = new JTextArea(h1NMR());
+			H1NMR.setEditable(false);
+			H1NMR.setLineWrap(true); 
+			H1NMR.setWrapStyleWord(true);
+			
+		NMRPanel.add(C13NMR);
+		NMRPanel.add(H1NMR);
 		return NMRPanel;
 	}
 	
@@ -172,20 +320,19 @@ public class MetaboliteInfoGUI implements Plugin
 		// TODO if datanode type equals metabolite{
 				
 		// create side bar
-		JPanel scroller = new JPanel();
+		JPanel panel = new JPanel();
 		
-		scroller.setLayout (new BoxLayout(scroller, BoxLayout.PAGE_AXIS));
-		scroller.setBackground(Color.white);
-		//scroller.add (new JLabel ("Metabolite information will be shown here."), BorderLayout.LINE_START);
-		scroller.add(GeneralPanel());
-		scroller.add(MSPanel());
-		scroller.add(NMRPanel());
+		panel.setLayout (new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		//panel.setBackground(Color.white);
+		panel.add(GeneralPanel());
+		panel.add(MSPanel());
+		panel.add(NMRPanel());
 		
 		// get a reference to the sidebar
 		JTabbedPane sidebarTabbedPane = desktop.getSideBarTabbedPane();
-		
+		JScrollPane jsp = new JScrollPane(panel);
 		// add side bar title
-		sidebarTabbedPane.add("Metabolite Information", scroller);
+		sidebarTabbedPane.add("Metabolite Information", jsp);
 		
 	}
 
