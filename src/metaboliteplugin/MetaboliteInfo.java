b199d969752302f5ea351335acd050fe8e0bcc6d
Rianne Fijten <riannefijten@gmail.com>
2013-06-25 13:15:29 +0200

    Added support for sparql and fixed bug

--- src/metaboliteplugin/MetaboliteInfo.java
+++ src/metaboliteplugin/MetaboliteInfo.java
@@ -4,24 +4,40 @@
 
 package metaboliteplugin;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import javax.management.Query;
 import javax.swing.JEditorPane;
 import javax.swing.SwingUtilities;
 import javax.swing.text.html.HTMLEditorKit;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import net.sf.jniinchi.JniInchiInput;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.bridgedb.IDMapper;
 import org.bridgedb.IDMapperException;
@@ -42,6 +58,7 @@ import org.pathvisio.core.Engine;
 import org.pathvisio.core.Engine.ApplicationEventListener;
 import org.pathvisio.core.data.GdbManager;
 import org.pathvisio.core.debug.Logger;
+import org.pathvisio.core.model.ObjectType;
 import org.pathvisio.core.model.PathwayElement;
 import org.pathvisio.core.model.PathwayElementEvent;
 import org.pathvisio.core.model.PathwayElementListener;
@@ -52,6 +69,10 @@ import org.pathvisio.core.view.SelectionBox.SelectionListener;
 import org.pathvisio.core.view.VPathway;
 import org.pathvisio.core.view.VPathwayElement;
 import org.pathvisio.gui.SwingEngine;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 
 public class MetaboliteInfo extends JEditorPane implements SelectionListener, PathwayElementListener, ApplicationEventListener
@@ -67,7 +88,6 @@ public class MetaboliteInfo extends JEditorPane implements SelectionListener, Pa
 	private ExecutorService executor;
 
 	PathwayElement input;
-	
 	final static int maxThreads = 1;
 	volatile ThreadGroup threads;
 	volatile Thread lastThread;
@@ -110,7 +130,7 @@ public class MetaboliteInfo extends JEditorPane implements SelectionListener, Pa
 	public void setInput(final PathwayElement e) 
 	{
 		//System.err.println("===== SetInput Called ==== " + e);
-		
+		//System.out.println("pathwayelement e: " + input);
 		if(e == input) return; //Don't set same input twice
 		
 		//Remove pathwaylistener from old input
@@ -218,6 +238,7 @@ public class MetaboliteInfo extends JEditorPane implements SelectionListener, Pa
 	public String getContent(PathwayElement e){
 			builder.setLength(0);
 			Xref ref = e.getXref();
+			//System.out.println("ref: " + ref);
 			IDMapper gdb = gdbManager.getCurrentGdb();		
 			Set<Xref> destrefs = null;
 			try {
@@ -258,6 +279,18 @@ public class MetaboliteInfo extends JEditorPane implements SelectionListener, Pa
 					//NMR();
 					HOSEGenerator(molecule);
 					
+					
+//					String PathwayName;
+//					System.out.println("PathwayName2: ");
+//					String DataNode = e.toString(); String DataNode2 = DataNode.replace("org.pathvisio.core.model.PathwayElement@", "");
+//					String endpoint = "sparql.wikipathways.org";
+//					String queryString = "select * where {?s ?p ?o . FILTER regex(str(?s), \"^http://rdf.wikipathways.org/Pathway/" + PathwayName + "/DataNode/" + DataNode2 + ")}";
+//					String user = null;
+//					String password = null;
+//					sparqlRemoteNoJena(endpoint, queryString, user, password);
+					
+					
+					
 					//Add databases that were used.
 					builder.append("<p> Databases used: <br />" +
 							"<sup>1</sup> <a href=\"http://cactus.nci.nih.gov/chemical/structure\"> Cactus Chemical Identifier Resolver </a><br />" +
@@ -271,6 +304,12 @@ public class MetaboliteInfo extends JEditorPane implements SelectionListener, Pa
 					ex.printStackTrace();
 					String str = "This HMDB ID was not recognized";
 					return str;
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					System.out.println("Exception e1");
+					String str = "The associated ID was not recognised by BridgeDB.";
+					return str;
 				}
 			return builder.toString();
 			}
@@ -283,7 +322,7 @@ public class MetaboliteInfo extends JEditorPane implements SelectionListener, Pa
 	public void CDKInfo(){
 		IMolecularFormula molForm = MolecularFormulaManipulator.getMolecularFormula(molecule);
 		String mf = MolecularFormulaManipulator.getString(molForm);
-		System.out.println(mf);
+		//System.out.println(mf);
 		builder.append("<tr><td>Molecular formula: </td><td>" + mf + "</td></tr>");
 
 	}
@@ -293,7 +332,7 @@ public class MetaboliteInfo extends JEditorPane implements SelectionListener, Pa
 //		InChIGenerator generator = null;
 //		System.out.println("inchi 1");
 		JniInchiInput input = new JniInchiInput();
-		System.out.println(input);
+		//System.out.println(input);
 		
 	}
 
@@ -449,7 +488,7 @@ public class MetaboliteInfo extends JEditorPane implements SelectionListener, Pa
 				try {
 					hoseCode = hcg.getHOSECode(mc, mc.getAtom(f), 1);
 					hoseCode = hcg.makeBremserCompliant(hoseCode);
-					System.out.println(hoseCode);
+					//System.out.println(hoseCode);
 					shift = predictor.predict(hoseCode);
 
 					List<IAtom> neighborList = mc.getConnectedAtomsList(atom);
@@ -469,5 +508,63 @@ public class MetaboliteInfo extends JEditorPane implements SelectionListener, Pa
 		}
 		builder.append("</table>");
 	}
+	
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////SPARQL ENDPOINT/////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+	//String PathwayName;
+	//String DataNodeCode = input.toString();	
+	
+	
+	
+	
+	public static String sparqlRemoteNoJena(String endpoint, String queryString, String user, String password)
+			  throws Exception {
+			      String s = null;
 
+			      // use Apache for doing the SPARQL query
+			      DefaultHttpClient httpclient = new DefaultHttpClient();
+
+			      
+			      // TEST TEST
+			      System.out.println("endpoint: " + endpoint);
+			      System.out.println("queryString: " + queryString);
+			      
+			      // Set credentials on the client
+			      if (user != null) {
+			    	  URL endpointURL = new URL(endpoint);
+			    	  CredentialsProvider credsProvider = new BasicCredentialsProvider();
+			    	  credsProvider.setCredentials(
+			            new AuthScope(endpointURL.getHost(), AuthScope.ANY_PORT), 
+			            new UsernamePasswordCredentials(user, password)
+			          );
+			    	  httpclient.setCredentialsProvider(credsProvider);
+			      }
+			         
+			      List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+			      formparams.add(new BasicNameValuePair("query", queryString));
+			      UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, "UTF-8");
+			      HttpPost httppost = new HttpPost(endpoint);
+			      httppost.setEntity(entity);
+			      HttpResponse response = httpclient.execute(httppost);
+			      HttpEntity responseEntity = response.getEntity();
+			      InputStream in = responseEntity.getContent();
+	
+			      // create a new DocumentBuilderFactory
+			      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+			      try {
+			         // use the factory to create a documentbuilder
+			         DocumentBuilder builder = factory.newDocumentBuilder();
+	
+			         // create a new document from input source
+			         Document doc = builder.parse(in);
+			         Element root = doc.getDocumentElement();
+			         s = root.toString();
+			      } catch (Exception ex) {
+			         ex.printStackTrace();
+			      }
+			      in.close();
+			      return s;
+		   }
 }
